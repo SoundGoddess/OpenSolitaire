f4a5665ed3bcd9615cca32cf98efe1abe2e60784
hathor <hathor@soundgoddess.me>
2016-04-01 15:54:58 -0700

    Initial Commit

---
+++ OpenSolitaireMG/OpenSolitaireMG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpenSolitaireMG {
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
