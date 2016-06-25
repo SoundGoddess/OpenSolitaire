ba77fd5b9163e3b1c47a0d54da87420ccf71fc01
kati <infinity@soundgoddess.me>
2016-06-25 01:32:06 -0700

    Unlicensing everything

--- Source/MonoGame.Ruge/CardEngine/Deck.cs
+++ Source/MonoGame.Ruge/CardEngine/Deck.cs
@@ -1,5 +1,5 @@
-﻿/* Attribution (a) 2016 The Ruge Project (http://ruge.metasmug.com/) 
- * Licensed under NWO-CS (see License.txt)
+/* Attribution (a) 2016 The Ruge Project (http://ruge.metasmug.com/) 
+ * Unlicensed under NWO-CS (see UNLICENSE)
  */
 
 using System;
